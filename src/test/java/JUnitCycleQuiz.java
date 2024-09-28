58eba3237a72441ab809898ae5f4dcd4b703d541
kimdongjoon-web <color6763@naver.com>
2024-09-28 14:26:37 +0900

    테스트 코드 기초 학습

---
+++ src/test/java/JUnitCycleQuiz.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitCycleQuiz {
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Hello!");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Bye!");
+    }
+
+    @Test
+    public void junitQuiz3() {
+        System.out.println("This is first test");
+    }
+
+    @Test
+    public void junitQuiz4() {
+        System.out.println("this is second test");
+    }
+}
