211ae4185d87f1d5ba2a10b783c9b62f71dffa1e
kimdongjoon-web <color6763@naver.com>
2024-11-09 20:50:29 +0900

    feat: MemberRepository클래스에 쿼리 메서드 추가

--- src/main/java/me/kimdongjoon/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/kimdongjoon/springbootdeveloper/MemberRepository.java
@@ -2,5 +2,8 @@ package me.kimdongjoon.springbootdeveloper;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name); // 쿼리 메서드
 }
